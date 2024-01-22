515507084b88865d9ad8200f6a8c3d151fed624b
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-01-22 17:49:05 +0100

    fix typo

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,7 +4,4 @@
 
 def insert_school(mongo_collection, **kwargs):
     """a Python function that create a new doc"""
-    new_doc = {}
-    for key, value in kwargs.items():
-        new_doc[key] = value
-    return mongo_collection.insert_one(new_doc)
+    return mongo_collection.insert_one(kwargs).inserted_id
