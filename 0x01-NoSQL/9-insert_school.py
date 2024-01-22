b6e3a167a9bb0305e24a646bb57784b13a373f8e
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-01-22 17:45:23 +0100

    insert new doc

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this function create a new doc"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """a Python function that create a new doc"""
+    new_doc = {}
+    for key, value in kwargs.items():
+        new_doc[key] = value
+    mongo_collection.insert_one(new_doc)
