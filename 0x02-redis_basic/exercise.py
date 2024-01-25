21a47d28924a970178291d421d27a717427505c4
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-01-25 16:55:17 +0100

    fix style

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,4 +1,4 @@
-#!/bin/usr/env python3
+#!/usr/bin/env python3
 """create class to instantiate redis"""
 import redis
 import uuid
@@ -8,10 +8,12 @@ class Cache:
     """class Cache that make a cash mem"""
 
     def __init__(self):
+        """main constructor"""
         self._redis = redis.Redis()
         self._redis.flushdb
 
     def store(self, data):
+        """store in redis"""
         key = uuid.uuid4()
         self._redis.set(str(key), str(data) if isinstance(data, str) else data)
         return str(key)
