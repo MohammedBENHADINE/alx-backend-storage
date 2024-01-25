1e9ab68d34bf25bb94a5a4a3928ea252f1bb568b
Mohammed BENHADINE <mohammedbenhadine@gmail.com>
2024-01-25 16:58:54 +0100

    fix typo

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,7 +11,7 @@ class Cache:
     def __init__(self):
         """main constructor"""
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store in redis"""
